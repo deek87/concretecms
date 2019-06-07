bd0f41b461d3fd708bcae755310764a2a950d9a6
Derek Cameron <derek@concrete5.co.jp>
2019-06-08 00:51:04 +0900

    Fixing php-fpm

--- .travis/setup-apache.sh
+++ .travis/setup-apache.sh
@@ -2,14 +2,16 @@
 # enable php-fpm
 apt-get update
 apt-get install -y apache2 libapache2-mod-fastcgi
-cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
+echo "PHP - $TRAVIS_PHP_VERSION"
+cp ~/.phpenv/versions/$TRAVIS_PHP_VERSION/etc/php-fpm.conf.default ~/.phpenv/versions/$TRAVIS_PHP_VERSION/etc/php-fpm.conf
 a2enmod rewrite actions fastcgi alias proxy_fcgi
-echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$TRAVIS_PHP_VERSION/etc/php.ini
 sed -i -e "s,www-data,travis,g" /etc/apache2/envvars
 chown -R travis:travis /var/lib/apache2/fastcgi
- ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
+ ~/.phpenv/versions/$TRAVIS_PHP_VERSION/sbin/php-fpm
 # configure apache virtual hosts
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 cp -f $SCRIPT_DIR/concrete5.conf /etc/apache2/sites-available/000-default.conf
 sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/000-default.conf
 service apache2 restart
+service php-fpm restart
