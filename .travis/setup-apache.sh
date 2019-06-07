4547a140c563429f03c02d4ef5c02df09ffb1bdc
Derek Cameron <derek@concrete5.co.jp>
2019-06-07 17:50:55 +0900

    Forgot install

--- .travis/setup-apache.sh
+++ .travis/setup-apache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # enable php-fpm
-apt-get apache2 libapache2-mod-fastcgi
+apt-get install apache2 libapache2-mod-fastcgi
 cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 a2enmod rewrite actions fastcgi alias proxy_fcgi
 echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
@@ -8,6 +8,7 @@ sed -i -e "s,www-data,travis,g" /etc/apache2/envvars
 chown -R travis:travis /var/lib/apache2/fastcgi
  ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
 # configure apache virtual hosts
-cp -f ./travis/concrete5.conf /etc/apache2/sites-available/000-default.conf
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+cp -f $SCRIPT_DIR/concrete5.conf /etc/apache2/sites-available/000-default.conf
 sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/000-default.conf
 service start apache2
