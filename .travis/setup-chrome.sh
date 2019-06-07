af2af9f73ebc4dc52edaa380ac0ecdae37421484
Derek Cameron <derek@concrete5.co.jp>
2019-06-08 00:02:59 +0900

    chmod chromedriver

--- .travis/setup-chrome.sh
+++ .travis/setup-chrome.sh
@@ -3,6 +3,7 @@ echo "Download chrome driver and unzip it"
 wget -c -nc --retry-connrefused --tries=0 http://chromedriver.storage.googleapis.com/74.0.3729.6/chromedriver_linux64.zip
 unzip -o -q chromedriver_linux64.zip
 echo "unzip complete"
+chmod 755 chromedriver
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 sleep 3 # give xvfb some time to start
