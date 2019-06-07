21d4881232bb73ac35ababeaa123043e1e0b227b
Derek Cameron <derek@concrete5.co.jp>
2019-06-07 18:09:24 +0900

    Change again for apache

--- .travis/setup-apache.sh
+++ .travis/setup-apache.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # enable php-fpm
+apt-get update
+apt-get install -y apache2 libapache2-mod-fastcgi
 cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 a2enmod rewrite actions fastcgi alias proxy_fcgi
 echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
@@ -10,4 +12,4 @@ chown -R travis:travis /var/lib/apache2/fastcgi
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 cp -f $SCRIPT_DIR/concrete5.conf /etc/apache2/sites-available/000-default.conf
 sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/000-default.conf
-service start apache2
+service apache2 restart
