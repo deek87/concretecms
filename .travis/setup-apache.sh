e00bf0320b047bdfaf4cf1edf4b400658f437761
Derek Cameron <derek@concrete5.co.jp>
2019-06-08 20:56:31 +0900

    setup apache fpm

--- .travis/setup-apache.sh
+++ .travis/setup-apache.sh
@@ -11,12 +11,13 @@ echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$TRAVIS_PHP_VERSION/etc/php.in
 sed -i -e "s,www-data,travis,g" /etc/apache2/envvars
 chown -R travis:travis /var/lib/apache2/fastcgi
 echo "start php-fpm"
-service ~/.phpenv/versions/$TRAVIS_PHP_VERSION/sbin/php-fpm restart
-service php$TRAVIS_PHP_VERSION-fpm restart
+~/.phpenv/versions/$TRAVIS_PHP_VERSION/sbin/php-fpm
 # configure apache virtual hosts
 
 cp -f $SCRIPT_DIR/concrete5.conf /etc/apache2/sites-available/000-default.conf
 sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-available/000-default.conf
+a2enconf php7.2
+a2enconf php7.2-fpm
 service apache2 restart
 echo "CHECK C5 Response"
 curl concrete5-test.test
